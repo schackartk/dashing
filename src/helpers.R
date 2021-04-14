bc0d4d469e5b79411d11dbe9d610386a063716df
Kenneth Schackart <schackartk1@gmail.com>
2021-04-13 22:29:04 -0700

    move all dash_df wrangling to wrangle_dashes() in helpers.R

--- src/helpers.R
+++ src/helpers.R
@@ -20,3 +20,43 @@ as_hour <- function(stri) {
     as.duration() %>% 
     as.numeric("hours")
 }
+
+#' Wrangle dashes dataframe
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+wrangle_dashes <- function(df) {
+  df <- df %>%
+    dplyr::mutate(
+      day = weekdays(as.Date(df$date)),
+      .after = date)
+  
+  df$day <- factor(df$day,
+                   levels = c("Sunday", "Monday", "Tuesday",
+                              "Wednesday", "Thursday", "Friday",
+                              "Saturday"))
+  
+  df <- df %>% dplyr::mutate(
+    start = lubridate::ymd_hm(paste(df$date,
+                                    df$time_in),
+                              tz = "MST"),
+    end = lubridate::ymd_hm(paste(df$date,
+                                  df$time_out),
+                            tz = "MST"),
+    total_time = round(as.numeric(difftime(end,
+                                           start,
+                                           units = "hours")),
+                       digits = 2),
+    active_hr = as_hour(active_time),
+    dash_hr = as_hour(dash_time))
+  
+  df <- df %>%
+    dplyr::mutate(miles = odo_out - odo_in,
+                  .after = odo_out) %>% 
+    dplyr::mutate(per_mi = round(earnings / miles, 2),
+                  per_trip = round(earnings / deliveries, 2),
+                  per_hour = round(earnings / total_time, 2),
+                  .after = earnings)
+}
