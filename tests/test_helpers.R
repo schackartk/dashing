e6b7a53a7ef6abb50ca8503b2ad61e56fc5c278a
Kenneth Schackart <schackartk1@gmail.com>
2021-04-25 14:34:36 -0700

    test as_hour()

--- tests/test_helpers.R
+++ tests/test_helpers.R
@@ -1,6 +1,12 @@
 source('../src/helpers.R')
 
-test_that("as_dollar() works", {
-  expect_equal("$17.59", as_dollar("17.59"))
-  expect_equal("$17.59", as_dollar(17.59))
+test_that('as_dollar() works', {
+  expect_equal('$17.59', as_dollar("17.59"))
+  expect_equal('$17.59', as_dollar(17.59))
+})
+
+test_that('as_hour() works', {
+  expect_equal(3.5, as_hour('3:30'))
+  expect_equal(0.5, as_hour('0:30'))
+  expect_equal(1.5, as_hour('0:90'))
 })
