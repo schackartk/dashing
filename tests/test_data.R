0244b923438637ef29dcc5be68d2d534048662f5
Kenneth Schackart <schackartk1@gmail.com>
2021-04-25 15:08:32 -0700

    check data is where expected and both sheets import cleanly

--- tests/test_data.R
+++ tests/test_data.R
@@ -1,13 +1,27 @@
 source('../src/config.R')
 source('../src/helpers.R')
 
+test_that('data file is where expected', {
+  expect_equal(TRUE, file.exists(data_paths['excel_file']))
+})
+
 test_that('df_dashes imports cleanly', {
-  df_dashes <- read_excel(path = data_paths['excel_file'],
-                          sheet = data_paths['dashes'])
-  expect_equal(9, ncol(df_dashes))
+  df <- read_excel(path = data_paths['excel_file'],
+                   sheet = data_paths['dashes'])
+  expect_equal(9, ncol(df))
   
   df_col_names <- c('date', 'time_in', 'time_out',
                     'odo_in', 'odo_out', 'earnings',
                     'active_time', 'dash_time', 'deliveries')
-  expect_equal(df_col_names, colnames(df_dashes))
+  expect_equal(df_col_names, colnames(df))
+})
+
+test_that('df_deliveries imports cleanly', {
+  df <- read_excel(path = data_paths['excel_file'],
+                   sheet = data_paths['deliveries'])
+  expect_equal(5, ncol(df))
+  
+  df_col_names <- c('date', 'base_pay', 'peak_pay',
+                    'tip', 'place')
+  expect_equal(df_col_names, colnames(df))
 })
