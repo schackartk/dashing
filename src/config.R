8329577e57ba153d5add152e585a4fc637551147
Kenneth Schackart <schackartk1@gmail.com>
2021-08-09 10:35:09 -0700

    make paths relative to root

--- src/config.R
+++ src/config.R
@@ -1,5 +1,5 @@
 data_paths <- c(
-  'excel_file' = '../data/raw_data.xlsx',
+  'excel_file' = 'data/raw_data.xlsx',
   'dashes' = 'days',
   'deliveries' = 'deliveries',
-  'wrangled_data' = '../data/wrangled_data.Rdata')
+  'wrangled_data' = 'data/wrangled_data.Rdata')
