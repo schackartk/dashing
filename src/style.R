a71dcd71a4347e7345e8d7602ba68b8f8846e540
Kenneth Schackart <schackartk1@gmail.com>
2021-08-09 09:59:59 -0700

    remove colors, using theme

--- src/style.R
+++ src/style.R
@@ -3,12 +3,3 @@ nb_theme <- ggplot2::theme_light() +
     ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                    plot.caption = ggplot2::element_text(hjust = 0.5),
                    plot.subtitle = ggplot2::element_text(hjust = 0.5)))
-
-nb_colors <- c(
-  'header_blue' = '#62d0f5',
-  'sub_blue' = '#9fe4f5',
-  'header_green' = '#62f576',
-  'sub_green' = '#9ff5ab',
-  'header_red' = '#f56262',
-  'sub_red' = '#f59f9f'
-)
